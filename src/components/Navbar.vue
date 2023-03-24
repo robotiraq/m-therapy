3ad47de705477586010d3c3e70cc4fc9de877dfb
Ibrahim Ismail <ibrahim1998i@yahoo.com>
2023-03-24 04:18:12 +0300

    Basics

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="py-4">
+    <div class="m-auto">
+      <div class="flex flex-row justify-center items-center">
+        <img class="m-auto order-2" src="../assets/Logo.png" alt="" />
+        <div class="px-6">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-10 h-10"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,120 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="mx-6">
+      <div class="grid grid-cols-5 items-center">
+        <div class="text-center col-span-3">
+          <div
+            class="font-bold bg-gradient-to-r from-sky-800 to-red-400 inline-block text-transparent bg-clip-text"
+          >
+            <h1 class="text-4xl">Accessible</h1>
+            <h2 class="text-base">Every Where, Any time.</h2>
+          </div>
+          <div class="flex flex-row justify-around text-xs mt-2">
+            <div class="flex flex-row items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4 text-green-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+              <p>Reliable</p>
+            </div>
+            <div class="flex flex-row items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4 text-green-500"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M4.5 12.75l6 6 9-13.5"
+                />
+              </svg>
+              <p>Scientific proven methods</p>
+            </div>
+          </div>
+          <button
+            class="py-3 w-full bg-sky-900 text-white text-xs rounded mt-2 font-bold"
+          >
+            Let’s start
+          </button>
+        </div>
+        <div class="col-span-2">
+          <img src="../assets/Hero.png" alt="" />
+        </div>
+      </div>
+      <h2
+        class="text-xl font-bold uppercase mt-10 bg-gradient-to-r bg-clip-text from-sky-800 to-red-400 inline-block text-transparent"
+      >
+        How it works
+      </h2>
+      <div class="grid grid-cols-5 items-center my-2">
+        <div class="text-left col-span-3">
+          <h2
+            class="text-xl uppercase font-bold bg-gradient-to-r bg-clip-text from-sky-800 to-red-400 inline-block text-transparent"
+          >
+            Get started
+          </h2>
+          <p class="text-xs">
+            Simply answer a few questions to help us understand your mental
+            condition and provide you with the best consultation possible.
+          </p>
+        </div>
+        <div class="col-span-2">
+          <img src="../assets/1.png" alt="" />
+        </div>
+      </div>
+      <div class="grid grid-cols-5 items-center my-4">
+        <div class="text-left col-span-3">
+          <h2
+            class="text-xl uppercase font-bold bg-gradient-to-r bg-clip-text from-sky-800 to-red-400 inline-block text-transparent"
+          >
+            Therapist match
+          </h2>
+          <p class="text-xs">
+            We will contact you to provide you with information about your
+            health and to recommend the ideal candidate for a consulting
+            session.
+          </p>
+        </div>
+        <div class="col-span-2 order-first">
+          <img src="../assets/2.png" alt="" />
+        </div>
+      </div>
+      <div class="grid grid-cols-5 items-center my-4">
+        <div class="text-left col-span-3">
+          <h2
+            class="truncate text-xl uppercase font-bold bg-gradient-to-r bg-clip-text from-sky-800 to-red-400 inline-block text-transparent"
+          >
+            at your convenience
+          </h2>
+          <p class="text-xs">
+            Feel cosy at your own place and have your session whenever you want.
+          </p>
+        </div>
+        <div class="col-span-2">
+          <img src="../assets/3.png" alt="" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+  components: {},
+};
 </script>
